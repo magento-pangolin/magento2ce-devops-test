bbbe8b0b4c867eaa12b88b173aecb6e7f4669e95
Ian Meron <imeron@magento.com>
2017-09-05 16:25:10 -0500

    Initial commit

---
+++ generateTests.php
@@ -0,0 +1,7 @@
+<?php
+require 'tests/acceptance/_bootstrap.php';
+
+\Magento\AcceptanceTestFramework\Util\TestGenerator::getInstance()->createAllCestFiles();
+
+print "Tests have been generated";
+print "\n";
